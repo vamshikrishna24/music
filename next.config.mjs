450378f54f9ebac39de898f668f614c698186f09
vamshikrishna24 <vamshikrishna2124@gmail.com>
2024-05-14 20:16:00 +0530

    change to config file next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,20 @@ const nextConfig = {
     ],
   },
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' blob:; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; font-src 'self';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
